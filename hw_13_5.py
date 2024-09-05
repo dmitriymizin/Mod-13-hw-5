cc7cd50c46c12377e3d7b98723c21a3f05be9985
dmitriymizin <169042072+dmitriymizin@users.noreply.github.com>
2024-09-05 10:14:43 +0300

    Add files via upload

---
+++ hw_13_5.py
@@ -0,0 +1,54 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot,storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.row(button1, button2)
+# kb.add(button1)
+# kb.add(button2)
+
+@dp.message_handler(commands = ['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age_=message.text)
+    await message.answer('Введите свой рост')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth_=message.text)
+    await message.answer('Введите свой вес')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight_=message.text)
+    data = await state.get_data()
+    amount_calories = (10 * float(data['weight_'])) + (6.25 * float(data['growth_'])) - (5 * float(data['age_']) + 5)
+    await message.answer(f'Ваша норма калорий {amount_calories}')
+    await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
